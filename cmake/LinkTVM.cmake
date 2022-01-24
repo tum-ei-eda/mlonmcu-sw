8e3ea59d06140ff8f0437bdb0d713699bb4d86f7
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-01-24 07:12:36 +0100

    MLIF: small tvm refactoring

--- cmake/LinkTVM.cmake
+++ cmake/LinkTVM.cmake
@@ -1,4 +1,4 @@
-SET(TVM_DIR
+SET(TVM_SRC
     "/data/work/code/tvm"
     CACHE STRING "TVM source directory"
 )
@@ -7,23 +7,23 @@ SET(TVM_ALIGNMENT_BYTES 4)
 
 # cmake-format: off
 SET(TVM_HEADERS
-    ${TVM_DIR}/include
-    ${TVM_DIR}/3rdparty/dlpack/include
-    ${TVM_DIR}/apps/bundle_deploy
-    ${TVM_DIR}/src/runtime/crt/include
+    ${TVM_SRC}/include
+    ${TVM_SRC}/3rdparty/dlpack/include
+    ${TVM_SRC}/apps/bundle_deploy
+    ${TVM_SRC}/src/runtime/crt/include
 )
 # cmake-format: on
 
 IF(TVM_CRT_CONFIG_DIR)
     LIST(APPEND TVM_HEADERS ${TVM_CRT_CONFIG_DIR})
 ELSE()
-    LIST(APPEND TVM_HEADERS ${TVM_DIR}/apps/bundle_deploy/crt_config)
+    LIST(APPEND TVM_HEADERS ${TVM_SRC}/apps/bundle_deploy/crt_config)
 ENDIF()
 
 # cmake-format: off
 ADD_LIBRARY(tvm_static_rt STATIC
-    ${TVM_DIR}/src/runtime/crt/common/crt_runtime_api.c
-    ${TVM_DIR}/src/runtime/crt/memory/stack_allocator.c
+    ${TVM_SRC}/src/runtime/crt/common/crt_runtime_api.c
+    ${TVM_SRC}/src/runtime/crt/memory/stack_allocator.c
 )
 # cmake-format: on
 
@@ -37,15 +37,15 @@ TARGET_COMPILE_DEFINITIONS(tvm_static_rt PUBLIC
 
 # cmake-format: off
 ADD_LIBRARY(tvm_graph_rt STATIC
-    ${TVM_DIR}/src/runtime/crt/common/crt_backend_api.c
-    ${TVM_DIR}/src/runtime/crt/common/crt_runtime_api.c
-    ${TVM_DIR}/src/runtime/crt/common/func_registry.c
-    ${TVM_DIR}/src/runtime/crt/memory/page_allocator.c
-    ${TVM_DIR}/src/runtime/crt/memory/stack_allocator.c
-    ${TVM_DIR}/src/runtime/crt/common/ndarray.c
-    ${TVM_DIR}/src/runtime/crt/common/packed_func.c
-    ${TVM_DIR}/src/runtime/crt/graph_executor/graph_executor.c
-    ${TVM_DIR}/src/runtime/crt/graph_executor/load_json.c
+    ${TVM_SRC}/src/runtime/crt/common/crt_backend_api.c
+    ${TVM_SRC}/src/runtime/crt/common/crt_runtime_api.c
+    ${TVM_SRC}/src/runtime/crt/common/func_registry.c
+    ${TVM_SRC}/src/runtime/crt/memory/page_allocator.c
+    ${TVM_SRC}/src/runtime/crt/memory/stack_allocator.c
+    ${TVM_SRC}/src/runtime/crt/common/ndarray.c
+    ${TVM_SRC}/src/runtime/crt/common/packed_func.c
+    ${TVM_SRC}/src/runtime/crt/graph_executor/graph_executor.c
+    ${TVM_SRC}/src/runtime/crt/graph_executor/load_json.c
 )
 # cmake-format: on
 
@@ -59,8 +59,8 @@ TARGET_COMPILE_DEFINITIONS(tvm_graph_rt PUBLIC
 
 # cmake-format: off
 ADD_LIBRARY(tvm_aot_rt STATIC
-    ${TVM_DIR}/src/runtime/crt/common/crt_backend_api.c
-    ${TVM_DIR}/src/runtime/crt/memory/stack_allocator.c
+    ${TVM_SRC}/src/runtime/crt/common/crt_backend_api.c
+    ${TVM_SRC}/src/runtime/crt/memory/stack_allocator.c
 )
 # cmake-format: on
 
