c7b94811f2cbade43b345a5424c261287882d4d9
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-02-11 11:25:57 +0100

    TVM: restore support for unpacked api

--- cmake/frameworks/tvm_mlif.cmake
+++ cmake/frameworks/tvm_mlif.cmake
@@ -1,11 +1,11 @@
-SET(TVM_OUT_DIR ${SRC_DIR}/codegen/host/src)
+SET(TVM_OUT_DIR ${SRC_DIR}/codegen/host/)
 SET(EXTRA_SRC ml_interface_tvm.c)
 
 FILE(
     GLOB
     TVM_SRCS
-    ${TVM_OUT_DIR}/*_lib*.c
-    ${TVM_OUT_DIR}/*_lib*.cc
+    ${TVM_OUT_DIR}/src/*_lib*.c
+    ${TVM_OUT_DIR}/src/*_lib*.cc
 )
 
 # Need this in extra target to avoid circular dependency .
@@ -13,9 +13,9 @@ COMMON_ADD_LIBRARY(tvm_extension STATIC ${TVM_SRCS})
 
 SET(EXTRA_SRC ${EXTRA_SRC} ${SRC_DIR}/${TVM_WRAPPER_FILENAME})
 TARGET_LINK_LIBRARIES(${TVM_LIB} PUBLIC tvm_extension)
-SET(EXTRA_INC ${TVM_OUT_DIR})
+SET(EXTRA_INC ${TVM_OUT_DIR}/include)
 
-TARGET_INCLUDE_DIRECTORIES(tvm_extension PUBLIC ${TVM_HEADERS} ${TVM_OUT_DIR} ${SRC_DIR})
+TARGET_INCLUDE_DIRECTORIES(tvm_extension PUBLIC ${TVM_HEADERS} ${TVM_OUT_DIR}/include ${SRC_DIR})
 TARGET_LINK_LIBRARIES(tvm_extension PUBLIC m)
 TARGET_LINK_LIBRARIES(tvm_extension PUBLIC ${TVM_LIB})
 SET(EXTRA_LIBS tvm_extension ${TVM_LIB})
