1db49c79db42b1031a7dee486312c47681b9f781
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-08-03 18:38:17 +0200

    refactor riscv llvm cmake config

--- cmake/toolchains/llvm_host_x86.cmake
+++ cmake/toolchains/llvm_host_x86.cmake
@@ -2,9 +2,18 @@
 
 # Lets stick to standard .elf file ending for now set(CMAKE_EXECUTABLE_SUFFIX_C .elf)
 
-SET(CMAKE_C_COMPILER clang-14)
-SET(CMAKE_ASM_COMPILER clang-14)
-set(CMAKE_C_LINKER lld-13)
+INCLUDE(LookupClang)
+
+SET(CMAKE_C_COMPILER ${CLANG_EXECUTABLE})
+SET(CMAKE_CXX_COMPILER ${CLANG++_EXECUTABLE})
+SET(CMAKE_ASM_COMPILER ${CLANG_EXECUTABLE})
+# TODO: automatic lookup with find_program
+
+SET(LLVM_VERSION_MAJOR 14)  # TODO: should not be hardcoded
+
+IF(LLVM_VERSION_MAJOR LESS 13)
+    MESSAGE(FATAL_ERROR "LLVM version 13 or higher is required")
+ENDIF()
 # set(CMAKE_C_LINKER lld-13) # TODO(fabianpedd): doesnt work, need to use -fuse-ld=lld-13 instead
 
 # SET(CMAKE_C_FLAGS
