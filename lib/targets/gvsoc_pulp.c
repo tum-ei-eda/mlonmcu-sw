adf603ae5b4fb4f97543e2629c76a8f4c13bef33
Philipp van Kempen <philipp.van-kempen@tum.de>
2022-11-09 09:41:45 +0100

    gvsoc_pulp: get number of cycles and instructions

--- lib/targets/gvsoc_pulp.c
+++ lib/targets/gvsoc_pulp.c
@@ -1,112 +1,44 @@
 #include <stdio.h>
 
-/* How many cycles (rdcycle) per second (OVPsim and Spike). */
-#define RDCYCLE_PER_SECOND 100000000UL
+#define CSR_PULP_PCMR 0xCC1
+#define CSR_PULP_PCER 0xCC0
+#define CSR_PULP_PCCR0 0x780
 
-/**
- * @brief Returns the number of clock cycles executed by the processor.
- * Overflows after 42.9 seconds on a 100 MIPS and with CPI = 1 (OVPsim).
- */
-static inline uint32_t rdcycle(void) {
-#if defined(__riscv) || defined(__riscv__)
-  uint32_t cycles;
-  __asm__ volatile("rdcycle %0" : "=r"(cycles));
-  return cycles;
-#else
-  return 0;
-#endif
-}
+#define csr_write(csr, val)         \
+({                \
+  unsigned long __v = (unsigned long)(val);   \
+  __asm__ ("csrw " #csr ", %0" \
+            : : "rK" (__v)      \
+            : "memory");      \
+})
 
-/**
- * @brief Returns the full 64bit register cycle register, which holds the
- * number of clock cycles executed by the processor.
- */
-static inline uint64_t rdcycle64() {
-#if defined(__riscv) || defined(__riscv__)
-  uint32_t cycles;
-  uint32_t cyclesh1;
-  uint32_t cyclesh2;
+#define csr_read(csr)           \
+({                \
+  register unsigned long __v;       \
+  __asm__ ("csrr %0, " #csr  \
+            : "=r" (__v) :      \
+            : "memory");      \
+  __v;              \
+  })
 
-  /* Reads are not atomic. So ensure, that we are never reading inconsistent
-   * values from the 64bit hardware register. */
-  do {
-    __asm__ volatile("rdcycleh %0" : "=r"(cyclesh1));
-    __asm__ volatile("rdcycle %0" : "=r"(cycles));
-    __asm__ volatile("rdcycleh %0" : "=r"(cyclesh2));
-  } while (cyclesh1 != cyclesh2);
 
-  return (((uint64_t)cyclesh1) << 32) | cycles;
-#else
-  return 0;
-#endif
-}
-
-/* How many time cycles (rdtime) per second (OVPsim and Spike). */
-#define RDTIME_PER_SECOND 1000000UL
-
-/**
- * @brief Returns wall-clock real time that has passed from an arbitrary start time in the past.
- */
-static inline uint32_t rdtime(void) {
-#if defined(__riscv) || defined(__riscv__)
-  uint32_t time;
-  __asm__ volatile("rdtime %0" : "=r"(time));
-  return time;
-#else
-  return 0;
-#endif
-}
-
-/**
- * @brief Returns the number of instructions retired by the processor.
- */
-static inline uint32_t rdinstret(void) {
-#if defined(__riscv) || defined(__riscv__)
-  uint32_t instret;
-  __asm__ volatile("rdinstret %0" : "=r"(instret));
-  return instret;
-#else
-  return 0;
-#endif
-}
-
-/**
- * @brief Enables the vector extension (as well as the floating point extension).
- */
-static inline void enable_fext(void)
-{
-#if defined(__riscv) || defined(__riscv__)
-    __asm__ volatile("li t0, 1<<13 \n"
-                     "csrs mstatus, t0 \n" ::
-                         : "t0");
-#endif
-}
-
-/**
- * @brief Enables the vector extension (as well as the floating point extension).
- */
-static inline void enable_vext(void)
-{
-#if defined(__riscv) || defined(__riscv__)
-    __asm__ volatile("li t0, 1<<9+1<<13 \n"
-                     "csrs mstatus, t0 \n" ::
-                         : "t0");
-#endif
-}
-
-static uint64_t start_cycles = 0;
+static uint32_t start_cycles = 0;
+static uint32_t start_instructions = 0;
 
 void init_target() {
-  // enable_fext();
-#ifdef USE_VEXT
-  // enable_vext();
-#endif
-  start_cycles = rdcycle64();
+  csr_write(0xCC0, 0b11);
+  start_cycles = csr_read(0x780);
+  start_instructions = csr_read(0x781);
+  // printf("Start Cycles: %ld\n", start_cycles);
+  // printf("Start Instructions: %ld\n", start_instructions);
 }
 
 void deinit_target() {
-  uint64_t stop_cycles = rdcycle64();
-  uint64_t diff_cycles = stop_cycles - start_cycles;
-  float diff_ms = 0;  // unimplemented (see RDCYCLE_PER_SECOND)
-  printf("Total Cycles: %lld\n", stop_cycles - start_cycles);
+   uint32_t stop_cycles= csr_read(0x780);
+   uint32_t stop_instructions= csr_read(0x781);
+  // WARNING: 32bit ony, overflows after 4294967295 cycles (42s at 100MHz)
+  uint32_t diff_cycles = stop_cycles - start_cycles;
+  uint32_t diff_instructions = stop_instructions - start_instructions;
+  printf("Total Cycles: %ld\n", diff_cycles);
+  printf("Total Instructions: %ld\n", diff_instructions);
 }
