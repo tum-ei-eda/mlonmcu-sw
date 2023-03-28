ac092a4ceb8d81c9e7a26014912f7e83eb704a5b
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-03-28 23:16:20 +0200

    refactor riscv toolchain files

--- cmake/toolchains/gcc_etiss_pulpino.cmake
+++ cmake/toolchains/gcc_etiss_pulpino.cmake
@@ -1,3 +1,21 @@
+SET(RISCV_ELF_GCC_PREFIX
+    ""
+    CACHE PATH "install location for riscv-gcc toolchain"
+)
+SET(RISCV_ELF_GCC_BASENAME
+    "riscv64-unknown-elf"
+    CACHE STRING "base name of the toolchain executables"
+)
+SET(RISCV_ARCH
+    "rv32gc"
+    CACHE STRING "march argument to the compiler"
+)
+SET(RISCV_ABI
+    "ilp32d"
+    CACHE STRING "mabi argument to the compiler"
+)
+SET(TC_PREFIX "${RISCV_ELF_GCC_PREFIX}/bin/${RISCV_ELF_GCC_BASENAME}-")
+
 # Copy-pasted from ETISS repo
 SET(CMAKE_SYSTEM_NAME Generic)
 
@@ -17,3 +35,6 @@ SET(CMAKE_RANLIB ${TC_PREFIX}ranlib${EXE_EXT})
 SET(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
 SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -march=${RISCV_ARCH} -mabi=${RISCV_ABI}")
+
+ADD_DEFINITIONS(-march=${RISCV_ARCH})
+ADD_DEFINITIONS(-mabi=${RISCV_ABI})
