80d667130b88519f2b5572cc572bb84233b09427
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-08-06 08:08:51 +0200

    add polybench template

---
+++ lib/polybench/polybench.c
@@ -0,0 +1,10 @@
+#include "polybench_wrapper.h"
+
+int mlonmcu_init() {}
+int mlonmcu_deinit() {}
+
+int mlonmcu_run() {
+    int   argc = 0;
+    char *argv[1];
+    return polybench_main(argc, argv);
+}
