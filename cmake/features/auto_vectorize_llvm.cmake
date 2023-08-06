9ab660a5f7c790d54c307ead70ba6811fa2d2d58
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-08-06 18:24:36 +0200

    fix feature flags

--- cmake/features/auto_vectorize_llvm.cmake
+++ cmake/features/auto_vectorize_llvm.cmake
@@ -18,14 +18,12 @@
 #      --riscv-v-vector-bits-min=${VLEN} \
 #  "
 #  )
-SET(AUTO_VECTORIZE_FLAGS "-fvectorize -fslp-vectorize")
+# SET(AUTO_VECTORIZE_FLAGS)
+LIST(APPEND AUTO_VECTORIZE_FLAGS "-fvectorize" "-fslp-vectorize")
 IF(RISCV_VEXT)
     IF(RISCV_VLEN)
         # IF(RISCV_VLEN GREATER_EQUAL 128)
-            SET(AUTO_VECTORIZE_FLAGS "${AUTO_VECTORIZE_FLAGS} \
-                -mllvm \
-                -scalable-vectorization=preferred \
-            ")
+            LIST(APPEND AUTO_VECTORIZE_FLAGS "-mllvm -scalable-vectorization=preferred")
                 # TODO: expose to feature
                 # --riscv-v-vector-bits-min=${RISCV_VLEN} \
                 # --riscv-v-vector-bits-max=${RISCV_VLEN} \
@@ -33,35 +31,27 @@ IF(RISCV_VEXT)
     ENDIF()
 ENDIF()
 IF(RISCV_AUTO_VECTORIZE_FORCE_VECTOR_WIDTH)
-    SET(AUTO_VECTORIZE_FLAGS "${AUTO_VECTORIZE_FLAGS} \
-        -mllvm --force-vector-width=${RISCV_AUTO_VECTORIZE_FORCE_VECTOR_WIDTH} \
-    ")
+    LIST(APPEND AUTO_VECTORIZE_FLAGS "-mllvm --force-vector-width=${RISCV_AUTO_VECTORIZE_FORCE_VECTOR_WIDTH}")
 ENDIF()
 IF(RISCV_AUTO_VECTORIZE_FORCE_VECTOR_INTERLEAVE)
-    SET(AUTO_VECTORIZE_FLAGS "${AUTO_VECTORIZE_FLAGS} \
-        -mllvm --force-vector-interleave=${RISCV_AUTO_VECTORIZE_FORCE_INTERLEAVE_COUNT} \
-    ")
+    LIST(APPEND AUTO_VECTORIZE_FLAGS "-mllvm --force-vector-interleave=${RISCV_AUTO_VECTORIZE_FORCE_VECTOR_INTERLEAVE}")
 ENDIF()
 IF(DEFINED RISCV_AUTO_VECTORIZE_LOOP)
     IF(NOT RISCV_AUTO_VECTORIZE_LOOP)
-        SET(AUTO_VECTORIZE_FLAGS "${AUTO_VECTORIZE_FLAGS} \
-            -fno-vectorize \
-        ")
+        LIST(APPEND AUTO_VECTORIZE_FLAGS "-fno-vectorize")
     ENDIF()
 ENDIF()
 IF(DEFINED RISCV_AUTO_VECTORIZE_SLP)
     IF(NOT RISCV_AUTO_VECTORIZE_SLP)
-        SET(AUTO_VECTORIZE_FLAGS "${AUTO_VECTORIZE_FLAGS} \
-            -fno-slp-vectorize \
-        ")
+        LIST(APPEND AUTO_VECTORIZE_FLAGS "-fno-slp-vectorize")
     ENDIF()
 ENDIF()
 
-IF(RISCV_AUTO_VECTORIZE_VERBOSE)
-    SET(AUTO_VECTORIZE_FLAGS "${AUTO_VECTORIZE_FLAGS} \
-        -Rpass=loop-vectorize -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize \
-        -Rpass=slp-vectorize -Rpass-missed=slp-vectorize -Rpass-analysis=slp-vectorize \
-    ")
+IF(RISCV_AUTO_VECTORIZE_VERBOSE STREQUAL "loop")
+    LIST(APPEND AUTO_VECTORIZE_FLAGS "-Rpass=loop-vectorize" "-Rpass-missed=loop-vectorize" "-Rpass-analysis=loop-vectorize")
+ELSEIF(RISCV_AUTO_VECTORIZE_VERBOSE STREQUAL "slp")
+    LIST(APPEND AUTO_VECTORIZE_FLAGS "-Rpass=slp-vectorize" "-Rpass-missed=slp-vectorize" "-Rpass-analysis=slp-vectorize")
 ENDIF()
-SET(FEATURE_EXTRA_C_FLAGS "${FEATURE_EXTRA_C_FLAGS} ${AUTO_VECTORIZE_FLAGS}")
-SET(FEATURE_EXTRA_CXX_FLAGS "${FEATURE_EXTRA_CXX_FLAGS} ${AUTO_VECTORIZE_FLAGS}")
+MESSAGE(STATUS "AUTO_VECTORIZE_FLAGS=${AUTO_VECTORIZE_FLAGS}")
+LIST(APPEND FEATURE_EXTRA_C_FLAGS ${FEATURE_EXTRA_C_FLAGS} ${AUTO_VECTORIZE_FLAGS})
+LIST(APPEND FEATURE_EXTRA_CXX_FLAGS ${FEATURE_EXTRA_CXX_FLAGS} ${AUTO_VECTORIZE_FLAGS})
