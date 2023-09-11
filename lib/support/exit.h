311ee289240c5f7622feb61e33a09cdae544578b
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-09-12 00:40:53 +0200

    introduce mlonmcu_exit utiluty for targets not fully exposing the exit code

--- lib/support/exit.h
+++ lib/support/exit.h
@@ -1,8 +1,16 @@
 #ifndef SUPPORT_EXIT_H
 #define SUPPORT_EXIT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #define EXIT_MLIF_BASE (0x10)
 #define EXIT_MLIF_INVALID_SIZE (EXIT_MLIF_BASE + 1)
 #define EXIT_MLIF_MISSMATCH (EXIT_MLIF_BASE + 2)
+
+void mlonmcu_exit(int status) {
+    printf("MLONMCU EXIT: %d\n", status);
+    exit(status);
+}
 
 #endif  // SUPPORT_EXIT_H
