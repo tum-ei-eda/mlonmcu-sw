7f8ccd19fbbccc021165629edcf7cf5f6585d877
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-10-16 00:39:15 +0200

    define default target_cycles/target_instructions

--- lib/targets/default.c
+++ lib/targets/default.c
@@ -1,4 +1,8 @@
+#include <stdint.h>
+
 __attribute__((weak)) void init_target() {}
 __attribute__((weak)) void deinit_target() {}
 __attribute__((weak)) void start_timer() {}
 __attribute__((weak)) void stop_timer() {}
+__attribute__((weak)) uint64_t target_cycles() { return 0; }
+__attribute__((weak)) uint64_t target_instructions() { return 0; }
