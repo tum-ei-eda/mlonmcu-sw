8e80b3353d3f2cec70f305febbf48e48900aead0
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-11-22 11:53:05 +0100

    printing: introduce STRIP_STRINGS option

--- lib/support/printing.h
+++ lib/support/printing.h
@@ -3,7 +3,12 @@
 
 #include "target.h"
 // void mlonmcu_printf(const char* format, ...);
+
+#ifdef STRIP_STRINGS
+#define mlonmcu_printf(format, ...)
+#else
 #define mlonmcu_printf target_printf
+#endif  // STRIP_STRINGS
 
 #ifdef _DEBUG
 #define DBGPRINTF(format, ...) mlonmcu_printf(format, ##__VA_ARGS__)
