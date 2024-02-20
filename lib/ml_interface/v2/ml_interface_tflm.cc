487385412bbb846acdc8c7b667b418dd0d734457
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-02-20 10:40:57 +0100

    ml_interface/v2: move tflm to new api

--- lib/ml_interface/v2/ml_interface_tflm.cc
+++ lib/ml_interface/v2/ml_interface_tflm.cc
@@ -5,63 +5,55 @@
 #ifdef __cplusplus
 extern "C"
 #endif
-int mlonmcu_init() {
+int mlif_num_inputs() {
+  return model_inputs();
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int mlif_num_outputs() {
+  return model_outputs();
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void* mlif_input_ptr(int i) {
+  return model_input_ptr(i);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+void* mlif_output_ptr(int i) {
+  return model_output_ptr(i);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int mlif_input_sz(int i) {
+  return model_input_size(i);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int mlif_output_sz(int i) {
+  return model_output_size(i);
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int mlif_invoke() {
+  return model_invoke();
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int mlif_init() {
   return model_init();
-}
-
-#ifdef __cplusplus
-extern "C"
-#endif
-int mlonmcu_deinit() {
-  return 0;
-}
-
-#ifdef __cplusplus
-extern "C"
-#endif
-int mlonmcu_run() {
-  size_t remaining = NUM_RUNS;
-  int ret;
-  while (remaining) {
-    ret = model_invoke();
-    if (ret) {
-      return ret;
-    }
-    remaining--;
-  }
-  return ret;
-}
-
-#ifdef __cplusplus
-extern "C"
-#endif
-int mlonmcu_check() {
-  size_t input_num = 0;
-  int ret;
-  bool new_;
-  while (true) {
-    ret = mlif_request_input(model_input_ptr(input_num), model_input_size(input_num), &new_);
-    if (ret) {
-      return ret;
-    }
-    if (!new_) {
-      break;
-    }
-    if (input_num == model_inputs() - 1) {
-      ret = model_invoke();
-      if (ret) {
-        return ret;
-      }
-      for (size_t i = 0; i < model_outputs(); i++) {
-        ret = mlif_handle_result(model_output_ptr(i), model_output_size(i));
-        if (ret) {
-          return ret;
-        }
-      }
-      input_num = 0;
-    } else {
-      input_num++;
-    }
-  }
-  return ret;
 }
