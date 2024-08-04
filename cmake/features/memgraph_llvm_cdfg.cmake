c922db89cf5f1437522df6dc0811507dfe42d4b6
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-08-04 17:23:14 +0200

    add feature files

---
+++ cmake/features/memgraph_llvm_cdfg.cmake
@@ -0,0 +1,13 @@
+IF(MEMGRAPH_LLVM_CDFG)
+    set(CMAKE_C_COMPILER_WORKS TRUE)
+    set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+    IF(${TOOLCHAIN} STREQUAL "llvm")
+        LIST(APPEND CDFG_FLAGS "-mllvm -cdfg-enable=1")
+        # TODO: move to new feature
+        LIST(APPEND CDFG_FLAGS "-fbasic-block-sections=labels")
+    ENDIF()
+
+    LIST(APPEND FEATURE_EXTRA_C_FLAGS ${CDFG_FLAGS})
+    LIST(APPEND FEATURE_EXTRA_CXX_FLAGS ${CDFG_FLAGS})
+ENDIF()
