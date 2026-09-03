caa5edc5b75486e7adaff71795161224fe72adf3
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-09-03 13:00:02 +0200

    lib/ml_interface/v1/default_model_support/process_input.c: fix format strings

--- lib/ml_interface/v1/default_model_support/process_input.c
+++ lib/ml_interface/v1/default_model_support/process_input.c
@@ -7,7 +7,7 @@
 int mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz) {
   if (in_size != 0) {
     if (in_size != model_input_sz) {
-      DBGPRINTF("MLIF: Given input size (%lu) does not match model input buffer size (%lu)!\n", in_size,
+      DBGPRINTF("MLIF: Given input size (%zu) does not match model input buffer size (%zu)!\n", in_size,
                 model_input_sz);
       return EXIT_MLIF_INVALID_SIZE;
     }
